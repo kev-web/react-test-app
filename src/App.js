77ddb3a5201df8111b798724ffbcc1a3e343bd00
Altimann <kev-web@users.noreply.github.com>
2022-02-08 12:32:17 -0600

    I created a React App and deployed it to Github man!

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>MY SUPER COOL REACT APP!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>I'm deploying the basic React app to Github Pages</p>
         <a
           className="App-link"
           href="https://reactjs.org"
